00e3aaafe650e781bff07cce9cd07c781cb159fb
wak <wkwkwk0111@gmail.com>
2023-03-30 17:15:20 +0000

    feat: add logger

--- script.py
+++ script.py
@@ -3,6 +3,12 @@ import img2pdf
 import cv2
 import numpy as np
 from PIL import Image
+import logging
+
+
+logger = logging.getLogger()
+logger.addHandler(logging.StreamHandler())
+logger.setLevel(logging.INFO)
 
 
 RED_COLOR = (234, 51, 35)
@@ -10,33 +16,46 @@ BLUE_COLOR = (0, 0, 245)
 GREEN_COLOR = (0, 255, 0)
 BASE_COLOR = BLUE_COLOR
 COLOR_GAP = 50
+ALPHA = 0.7
 INPUT_PDF_PATH = './resources/document.pdf'
 OUTPUT_PDF_PATH = './var/output.pdf'
 
 
 def main():
+    logger.info(f'''
+    INPUT_PDF_PATH: {INPUT_PDF_PATH}
+    OUTPUT_PDF_PATH: {OUTPUT_PDF_PATH}
+    BASE_COLOR: {BASE_COLOR}
+    COLOR_GAP: {COLOR_GAP}
+    ''')
     page_images = convert_from_path(INPUT_PDF_PATH)
     output_images = []
-    index = 0
+    index = 1
     for page_image in page_images:
+        logger.info(f'Processing page {index}...')
         np_image = np.array(page_image)
         areas = pickup_color_areas(np_image, BASE_COLOR, COLOR_GAP)
         if len(areas) < 1:
             continue
-        canvas = np.clip(np.array(0.7 * np_image), 0, 255).astype(np.uint8)
-        # 各矩形領域をキャンバスに追加する
-        for x, y, w, h in areas:
-            roi = np_image[y:y+h, x:x+w]
-            canvas[y:y+h, x:x+w] = roi
-        output_image_path = './var/output_{}.png'.format(index)
-        Image.fromarray(canvas).save(output_image_path)
+        output_image_path = './var/output_{:03d}.png'.format(index)
+        highlight_areas(np_image, areas, output_image_path)
         output_images.append(output_image_path)
         index += 1
     if len(output_images) < 1:
+        logger.info('No area found. (No output file is generated.)')
         return
     with open(OUTPUT_PDF_PATH, 'wb') as f:
         f.write(img2pdf.convert(output_images))
+    logger.info(f'Done. (Output file {OUTPUT_PDF_PATH} is generated.)')
 
+def highlight_areas(np_image, areas, output_image_path):
+    canvas = np.clip(np.array(ALPHA * np_image), 0, 255).astype(np.uint8)
+        # 各矩形領域をキャンバスに追加する
+    for x, y, w, h in areas:
+        roi = np_image[y:y+h, x:x+w]
+        canvas[y:y+h, x:x+w] = roi
+    Image.fromarray(canvas).save(output_image_path)
+    return output_image_path
 
 def pickup_color_areas(np_image, color, gap):
     lower_color = tuple([max(0, i - gap) for i in color])
