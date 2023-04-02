5028e5f058ef09823c41c2a74eb8f4b18dc37d44
wak <wkwkwk0111@gmail.com>
2023-04-02 01:28:13 +0000

    feat: use cli arguments

--- script.py
+++ script.py
@@ -1,3 +1,5 @@
+import argparse
+
 from pdf2image import convert_from_path
 import img2pdf
 import cv2
@@ -11,46 +13,62 @@ logger.addHandler(logging.StreamHandler())
 logger.setLevel(logging.INFO)
 
 
-RED_COLOR = (234, 51, 35)
-BLUE_COLOR = (0, 0, 245)
-GREEN_COLOR = (0, 255, 0)
-BASE_COLOR = BLUE_COLOR
-COLOR_GAP = 50
-ALPHA = 0.7
 INPUT_PDF_PATH = './resources/document.pdf'
 OUTPUT_PDF_PATH = './var/output.pdf'
+RGB = '0,0,0'
+COLOR_GAP = 30
+SHADOW = 0.7
 
 
-def main():
-    logger.info(f'''
-    INPUT_PDF_PATH: {INPUT_PDF_PATH}
-    OUTPUT_PDF_PATH: {OUTPUT_PDF_PATH}
-    BASE_COLOR: {BASE_COLOR}
-    COLOR_GAP: {COLOR_GAP}
-    ''')
-    page_images = convert_from_path(INPUT_PDF_PATH)
+def main():    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input', type=str, default=INPUT_PDF_PATH,
+                        help='Input PDF file path. (default: ./resources/document.pdf)')
+    parser.add_argument('--output', type=str, default=OUTPUT_PDF_PATH,
+                        help='Output PDF file path. (default: ./var/output.pdf)')
+    parser.add_argument('--rgb', type=str, default=RGB,
+                        help='RGB color to highlight. (default: 0,0,0)')
+    parser.add_argument('--gap', type=int, default=COLOR_GAP,
+                        help='Color gap to highlight. (default: 50)')
+    parser.add_argument('--shadow', type=float, default=SHADOW,
+                        help='Shadow ratio. (default: 0.7)')
+
+    args = parser.parse_args()
+    rgb = tuple([int(i) for i in args.rgb.split(',')])
+    assert len(rgb) == 3, 'rgb must be 3 integers separated by comma.'
+    assert list(filter(lambda i: i < 0 or i > 255, rgb)) == [], 'rgb must be 3 integers between 0 and 255.'
+    
+    logger.info('\n'.join([
+        f'input: {args.input}',
+        f'output: {args.output}',
+        f'rgb: {rgb}',
+        f'gap: {args.gap}',
+        f'shadow: {args.shadow}',
+        '-' * 20
+    ]))
+    page_images = convert_from_path(args.input)
     output_images = []
-    index = 1
+    page = 1
     for page_image in page_images:
-        logger.info(f'Processing page {index}...')
+        logger.info(f'Processing page {page}...')
         np_image = np.array(page_image)
-        areas = pickup_color_areas(np_image, BASE_COLOR, COLOR_GAP)
+        areas = pickup_color_areas(np_image, rgb, args.gap)
         if len(areas) < 1:
             continue
-        output_image_path = './var/output_{:03d}.png'.format(index)
+        output_image_path = './var/output_{:03d}.png'.format(page)
         highlight_areas(np_image, areas, output_image_path)
         output_images.append(output_image_path)
-        index += 1
+        page += 1
     if len(output_images) < 1:
         logger.info('No area found. (No output file is generated.)')
         return
-    with open(OUTPUT_PDF_PATH, 'wb') as f:
+    with open(args.output, 'wb') as f:
         f.write(img2pdf.convert(output_images))
-    logger.info(f'Done. (Output file {OUTPUT_PDF_PATH} is generated.)')
+    logger.info(f'Done. (Output file {args.output} is generated.)')
 
 def highlight_areas(np_image, areas, output_image_path):
-    canvas = np.clip(np.array(ALPHA * np_image), 0, 255).astype(np.uint8)
-        # 各矩形領域をキャンバスに追加する
+    canvas = np.clip(np.array(SHADOW * np_image), 0, 255).astype(np.uint8)
+    # 各矩形領域をキャンバスに追加する
     for x, y, w, h in areas:
         roi = np_image[y:y+h, x:x+w]
         canvas[y:y+h, x:x+w] = roi
